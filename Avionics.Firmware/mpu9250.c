ee52065c2193a534696b6e1174181eae61fbaae1
Rich Wareham <rich@richwareham.com>
2016-03-13 16:02:07 +0000

    firmware:mpu9250: add gyro self-test to init sequence
    Re-jig mpu9250_init() to use a table of registers and values to make it a little
    easier to edit. Also call (and check return value of) mpu9250_self_test_gyro()
    to perform a self-test on the gyroscope.
    
    It's non-obvious where to find the application note referenced in the datasheet
    on self-testing. Instead I've cribbed from other MPU9250 drivers and implemented
    the same self-test algorithm.
    
    Essentially the driver now takes a mean measurement before and after self-test
    activation and subtracts them to get the "pure" self-test effect. Then the
    factory-set expected values are expanded out from the appropriate registers. If
    the self-test values differ from the expect values by more than 5% the self-test
    fails. I've chosen the 5% threshold somewhat arbitrarily.
    
    A little bit of work is done to minimise the number of floating point operations
    which need to be performed. Most of the work is kept in the integer domain.

--- Avionics.Firmware/mpu9250.c
+++ Avionics.Firmware/mpu9250.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <math.h> // for self-test
+
 #include "ch.h"
 #include "chprintf.h"
 #include "mpu9250.h"
@@ -30,6 +32,10 @@ static void mpu9250_write_u8(uint8_t addr, uint8_t val);
 // mpu9250_id_check performs a simple sanity check on MPU9250 communication by
 // checking that the WHO_AM_I register of the MPU9250 has an expected value.
 static bool mpu9250_id_check(void);
+
+// mpu9250_self_test_gyro performs a self-test on the gyro, compares the result
+// to the factory stored values and returns true iff the self-test succeeds.
+static bool mpu9250_self_test_gyro(void);
 
 static uint8_t mpu9250_read_u8(uint8_t addr) {
     // Set the read bit
@@ -106,14 +112,113 @@ static bool mpu9250_id_check(void) {
     return whoami == MPU9250_WHO_AM_I_RESET_VALUE;
 }
 
+// Take the mean of the three measurement values represented by the six
+// registers starting at base_reg. Write 16-bit results to the buffer pointed to
+// by out. The behaviour is to sample 256 values at 1KHz and return the mean.
+static void mpu9250_read_mean_triplet(uint8_t base_reg, int16_t *out) {
+    uint8_t sensor_values[6];
+    int32_t sensor_accumulator[3] = {0, 0, 0};
+
+    // Accumulate 256 readings
+    for(int i=0; i<256; ++i) {
+        // Read gyro values
+        mpu9250_read_multiple(base_reg, sensor_values, 6);
+
+        sensor_accumulator[0] += (int32_t)bytes_to_uint16(
+            sensor_values[0], sensor_values[1]);
+        sensor_accumulator[1] += (int32_t)bytes_to_uint16(
+            sensor_values[2], sensor_values[3]);
+        sensor_accumulator[2] += (int32_t)bytes_to_uint16(
+            sensor_values[4], sensor_values[5]);
+
+        chThdSleepMilliseconds(1);
+    }
+
+    // Shift right to take mean and record
+    for(int i=0; i<3; ++i) {
+        sensor_accumulator[i] >>= 8;
+        out[i] = sensor_accumulator[i];
+    }
+}
+
+// mpu9250_self_test_to_factory_trim takes self-test register values and
+// transforms them to the corresponding factory trim values which should match
+// the sensor readings. (It is assumed that the full-scale select is 00 ==
+// +/-250 deg/sec.) It appears non-trivial to find the application note which
+// describes the self-test procedure. This formula was lifted from [1].
+//
+// [1] https://github.com/kriswiner/MPU-9250/blob/master/MPU9250BasicAHRS.ino
+static inline int16_t mpu9250_self_test_to_factory_trim(uint8_t st_val) {
+    return (int16_t)(2620.f * powf(1.01f, ((float)st_val) - 1.f));
+}
+
+static bool mpu9250_self_test_gyro(void) {
+    uint8_t factory_st_output[3], old_gyro_config;
+    int16_t base_values[3], self_test_values[3];
+
+    // Read old gyro config
+    old_gyro_config = mpu9250_read_u8(MPU9250_REG_GYRO_CONFIG);
+
+    // Gyro config => range of +/- 250 degrees/sec
+    mpu9250_write_u8(MPU9250_REG_GYRO_CONFIG, 0x00);
+
+    // Delay for a short while to let device stabilise
+    chThdSleepMilliseconds(50);
+
+    // Record mean of incoming values
+    mpu9250_read_mean_triplet(MPU9250_REG_GYRO_XOUT_H, base_values);
+
+    // Set gyro config to self test on all axes with gyro range of +/- 250
+    // degrees/s
+    mpu9250_write_u8(MPU9250_REG_GYRO_CONFIG, 0xe0);
+
+    // Delay for a short while to let device stabilise
+    chThdSleepMilliseconds(50);
+
+    // Record mean of incoming values
+    mpu9250_read_mean_triplet(MPU9250_REG_GYRO_XOUT_H, self_test_values);
+
+    // Restore old gyro config
+    mpu9250_write_u8(MPU9250_REG_GYRO_CONFIG, old_gyro_config);
+
+    // Subtract base readings from self_test_values.
+    for(int i=0; i<3; ++i) {
+        self_test_values[i] -= base_values[i];
+    }
+
+    // Read factory self-test output
+    mpu9250_read_multiple(MPU9250_REG_SELF_TEST_X_GYRO, factory_st_output, 3);
+
+    // Convert this to expected values for self-test output and check absolute
+    // difference from measured. Fail if the difference is greater than 5% of
+    // the expected value.
+    for(int i=0; i<3; ++i) {
+        int16_t expected_val = mpu9250_self_test_to_factory_trim(
+            factory_st_output[i]);
+        int32_t delta = (int32_t)self_test_values[i] - (int32_t)expected_val;
+        if(abs(delta) > abs(expected_val)/20) {
+            // We fail :(
+            return false;
+        }
+    }
+
+    // Self test passed!
+    return true;
+}
+
 static void mpu9250_init(void) {
-    // Set User Control Register - Disable FIFO, Disable I2C
-    mpu9250_write_u8(0x6A, 0x10);
+    // Register/value pairs to reset/initialise MPU9250
+    uint8_t init_sequence[][2] = {
+        { MPU9250_REG_PWR_MGMT_1, 0x80 }, // Reset
+        { MPU9250_REG_PWR_MGMT_1, 0x00 }, // Select internal 20MHz source
+        { MPU9250_REG_PWR_MGMT_2, 0x00 }, // Enable gyro & accel
+        { MPU9250_REG_USER_CTRL, 0x10 }, // SPI only, sidable FIFO
+    };
 
-    // TODO: Initialize sensor - setting control registers to appropriate values
-
-
-    // TODO: Perform self test
+    // Perform initial reset
+    for(size_t i=0; i < sizeof(init_sequence)/sizeof(init_sequence[0]); ++i) {
+        mpu9250_write_u8(init_sequence[i][0], init_sequence[i][1]);
+    }
 }
 
 void mpu9250_wakeup(EXTDriver *extp, expchannel_t channel) {
@@ -151,6 +256,11 @@ msg_t mpu9250_thread(COMPILER_UNUSED_ARG(void *arg)) {
     bthandler_set_error(ERROR_MPU9250, false);
 
     mpu9250_init();
+
+    // Perform a self-test of the MPU9250
+    if(!mpu9250_self_test_gyro()) {
+        bthandler_set_error(ERROR_MPU9250, true);
+    }
 
     uint16_t raw_data[10];
     while(TRUE) {
