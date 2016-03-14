3f3c44c0b27a51cf215d6c73d149dd793b536bc6
Rich Wareham <rich@richwareham.com>
2016-03-14 19:58:36 +0000

    firmware:mpu9250: re-factor mpu9250_burst_read
    Convert mpu9250_burst_read to read data into the buffer in-place. Should we be
    building on a big-endian device, this is all we need to do. Otherwise, we need
    to swap bytes.

--- Avionics.Firmware/mpu9250.c
+++ Avionics.Firmware/mpu9250.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <math.h> // for self-test
+#include <machine/endian.h>
 
 #include "ch.h"
 #include "chprintf.h"
@@ -28,6 +29,12 @@ static void mpu9250_read_multiple(uint8_t addr, uint8_t* buf, int num);
 static void mpu9250_write_u8(uint8_t addr, uint8_t val);
 
 //// HIGH-LEVEL OPERATIONS ////
+
+// mpu9250_burst_read will grab all the sensor data and writes it to out which
+// must point to a buffer of at least 10 uint16_t-s. The values written are:
+// ACCEL_XOUT, ACCEL_YOUT, ACCEL_ZOUT, TEMP_OUT, GYRO_XOUT, GYRO_YOUT, GYRO_ZOUT
+// MAGNO_XOUT, MAGNO_YOUT, MAGNO_ZOUT
+static void mpu9250_burst_read(uint16_t *out);
 
 // mpu9250_id_check performs a simple sanity check on MPU9250 communication by
 // checking that the WHO_AM_I register of the MPU9250 has an expected value.
@@ -74,36 +81,24 @@ static void mpu9250_write_u8(uint8_t addr, uint8_t val) {
     spiUnselect(&MPU9250_SPID);
 }
 
-static void mpu9250_burst_read(uint16_t data_out[10]) {
-    // Grab all the sensor data in the minimal number of steps
-    // It also merges high and low bytes together for convenience
-    // The array contains the following in order
-    // ACCEL_XOUT, ACCEL_YOUT, ACCEL_ZOUT, TEMP_OUT, GYRO_XOUT, GYRO_YOUT, GYRO_ZOUT
-    // MAGNO_XOUT, MAGNO_YOUT, MAGNO_ZOUT
+static void mpu9250_burst_read(uint16_t *out) {
+    // Cast output to uint8_t buffer
+    uint8_t *out_u8 = (uint8_t*)out;
 
+    // Read accel, temp and gyro.
+    mpu9250_read_multiple(MPU9250_REG_ACCEL_XOUT_H, out_u8, 14);
 
-    // We first read accel and gyro data
-    uint8_t rx_buff[14];
+    // Read magno data
+    mpu9250_read_multiple(0x03, out_u8 + 14, 6);
 
-    mpu9250_read_multiple(0x3B, rx_buff, 14);
-
-    int j = 0;
-    for (int i = 0; i < 7; i++) {
-        // Merge high byte (which comes first) with low byte (which comes second)
-        data_out[i] = (rx_buff[j] << 7) | rx_buff[j + 1];
-        j += 2;
+    // If necessary, convert buffer to host endian-ness
+#if BYTE_ORDER != BIG_ENDIAN
+    for(int i=0; i<20; i+=2) {
+        uint8_t tmp = out_u8[i];
+        out_u8[i] = out_u8[i+1];
+        out_u8[i+1] = tmp;
     }
-
-    // We next get the magno data
-    mpu9250_read_multiple(0x03, rx_buff, 6);
-
-    j = 0;
-    for (int i = 7; i < 10; i++) {
-        // Merge high byte (which comes second) with low byte (which comes first)
-        data_out[i] = (rx_buff[j+1] << 7) | rx_buff[j];
-        j += 2;
-    }
-
+#endif
 }
 
 static bool mpu9250_id_check(void) {
