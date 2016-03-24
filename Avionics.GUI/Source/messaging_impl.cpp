45ad047192e3bbe6b8ff757fa51c06323c729722
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-03-24 14:45:53 +0000

    Added C++ implementation of messaging system. It is not tested though as the GUI won't compile yet.

--- Avionics.GUI/Source/messaging_impl.cpp
+++ Avionics.GUI/Source/messaging_impl.cpp
@@ -1,102 +1,154 @@
-#include <mutex>
-#include <TQueue.h>
 
 extern "C" {
-#include "messaging.h"
+    #include "messaging.h"
 }
 
-#define MAX_NUM_DELEGATES 10
+#include <string.h>
+#include "platform.h"
+#include <TQueue.h>
+#include <atomic>
 
-struct messaging_delegate_t {
-    messaging_delegate_func delegate_func;
+#define MAX_NUM_CONSUMERS 20
+#define MAX_NUM_PRODUCERS 20
 
-    TQueue<telemetry_t> mailbox;
-    messaging_delegate_t* next;
+#define MAX_NUM_TELEMETRY_REFS 1000
+
+struct TelemetryRef {
+    telemetry_t* packet;
+
+    TelemetryRef() {
+        packet = nullptr;
+    }
+
+    ~TelemetryRef() {
+        telemetry_allocator_free(packet);
+    }
 };
 
-// The number in the messaging_delegates_pool when registering the next delegate
-static volatile int cur_delegate_pool_index = 0;
+// This may be used for load tracking later on so is included
+struct message_producer_impl_t {
+    message_producer_t* parent;
+};
 
-// Delegates are allocated from here. This is so that the messaging component
-// 'owns' them (i.e. they won't get cleaned up for any reason)
-// It also hides the implementation details from any user of this component
-static messaging_delegate_t messaging_delegates_pool[MAX_NUM_DELEGATES];
+struct message_consumer_impl_t {
+    TQueue<std::shared_ptr<TelemetryRef>> mailbox;
+    message_consumer_t* parent;
+};
 
-// Used to make delegate registration synchronous
-// This protects cur_delegate_pool_index
-// This could be made more fine-grained but I see no major reason to
-static std::mutex messaging_register_mutex;
+static std::atomic<uint32_t> cur_consumer_pool_index = 0;
 
-// Maps each telemetry source to a linked list of messaging delegates
-static messaging_delegate_t* messaging_delegates[16];
+static message_consumer_impl_t consumer_pool[MAX_NUM_CONSUMERS];
 
-static std::mutex messaging_delegates_locks[16];
+static std::mutex consumer_register_mutex;
 
-extern "C" void messaging_init(void) {
-    for (int i = 0; i < 16; i++) {
-        messaging_delegates[i] = nullptr; 
-    }
+static std::atomic<uint32_t> cur_producer_pool_index = 0;
+
+static message_producer_impl_t producer_pool[MAX_NUM_PRODUCERS];
+
+static std::mutex producer_register_mutex;
+
+void init_messaging(void) {
+
 }
 
-extern "C" bool messaging_register_delegate(message_delegate_id* delegate_id, messaging_delegate_func func, telemetry_source_t source, uint32_t buffer_size) {
-    *delegate_id = 255;
-    messaging_delegate_t* delegate;
-    {
-        std::lock_guard<std::mutex> lock(messaging_register_mutex);
+// Initialise a producer - returns false on error
+extern "C" bool messaging_producer_init(message_producer_t* producer) {
+    if (producer->impl != nullptr)
+        return true; // We assume it has already been initialised
 
-        if (cur_delegate_pool_index >= MAX_NUM_DELEGATES || source >= 16) {
-            return false; // TODO: Log some sort of error
+    std::lock_guard<std::mutex> lock(producer_register_mutex);
+    if (cur_producer_pool_index >= MAX_NUM_PRODUCERS) {
+        return false;
+    }
+    producer->impl = &producer_pool[cur_producer_pool_index];
+
+    // Perform any initialisation
+    producer->impl->parent = producer;
+    telemetry_allocator_init(producer->telemetry_allocator);
+
+    // We don't need the thread safe version
+    // As stores are atomic and we only modify within this lock zone
+    ++cur_producer_pool_index; // NB: this must be done after all initialization
+    return true;
+}
+
+// Initialise a consumer - returns false on error
+extern "C" bool messaging_consumer_init(message_consumer_t* consumer) {
+    if (consumer->impl != nullptr)
+        return true; // We assume it has already been initialised
+
+    std::lock_guard<std::mutex> lock(consumer_register_mutex);
+    if (cur_consumer_pool_index >= MAX_NUM_CONSUMERS) {
+        return false;
+    }
+    consumer->impl = &consumer_pool[cur_consumer_pool_index];
+    consumer->impl->parent = consumer;
+
+    // Perform any initialisation
+
+    // We don't need the thread safe version
+    // As stores are atomic and we only modify within this lock zone
+    ++cur_consumer_pool_index; // NB: this must be done after all initialization
+    return true;
+}
+
+
+static bool messaging_consumer_enqueue_packet(message_consumer_t* consumer, const std::shared_ptr<TelemetryRef>& ref) {
+    consumer->impl->mailbox.enqueue(ref);
+    return true;
+}
+
+// Send a mesage from the specified producer
+// A copy of the data will be made, so you can freely modify/release the data after this call
+extern "C" messaging_send_return_codes messaging_producer_send(message_producer_t* producer, uint16_t tag, const uint8_t* data, uint16_t length) {
+    if (producer->impl == NULL)
+        return messaging_send_invalid_producer;
+    if ((tag & producer->packet_source_mask) != 0)
+        return messaging_send_invalid_tag;
+
+    auto ref = std::make_shared<TelemetryRef>();
+
+    telemetry_t* packet = telemetry_allocator_alloc(producer->telemetry_allocator, length);
+    ref->packet = packet;
+    if (packet == nullptr) {
+        return messaging_send_producer_heap_full;
+    }
+    memcpy(packet->payload, data, length);
+
+    // We have already checked the tag and source don't overlap earlier
+    packet->header.id = tag | producer->packet_source;
+    packet->header.length = length;
+    packet->header.timestamp = platform_get_counter_value();
+    packet->header.origin = TELEMETRY_ORIGIN;
+
+    bool enqueue_successful = true;
+    // We create a local copy as it frees up the compiler
+    // If a consumer register during this call it isn't a massive deal that
+    // we won't pass it the packet
+    uint32_t num_consumers = cur_consumer_pool_index;
+    for (uint32_t i = 0; i < num_consumers; ++i) {
+        message_consumer_t* consumer = consumer_pool[i].parent;
+        if ((consumer->packet_source_mask & packet->header.id) == consumer->packet_source
+            && !messaging_consumer_enqueue_packet(consumer, ref)) {
+            enqueue_successful = false;
         }
-
-
-        delegate = &messaging_delegates_pool[cur_delegate_pool_index];
-        *delegate_id = cur_delegate_pool_index;
-        cur_delegate_pool_index++;
-    }
-    
-
-    delegate->delegate_func = func;
-
-    {
-        std::lock_guard<std::mutex> source_lock(messaging_delegates_locks[source]);
-        delegate->next = messaging_delegates[source];
-        messaging_delegates[source] = delegate;
-    }
-    return true;
-}
-
-static void messaging_send_message_to_delegate(telemetry_t* message, messaging_delegate_t* delegate) {
-    delegate->mailbox.enqueue(*message);
-}
-
-static void messaging_send_message_to_source(telemetry_t* message, telemetry_source_t source) {
-    std::lock_guard<std::mutex> lock(messaging_delegates_locks[source]);
-    messaging_delegate_t* current_delegate = messaging_delegates[source];
-    while (current_delegate != nullptr) {
-        messaging_send_message_to_delegate(message, current_delegate);
-        current_delegate = current_delegate->next;
-    }
-}
-
-extern "C" void messaging_send_message(telemetry_t* message) {
-    messaging_send_message_to_source(message, (telemetry_source_t)message->source);
-    messaging_send_message_to_source(message, telemetry_source_wildcard);
-}
-
-
-extern "C" bool messaging_process_message(message_delegate_id* delegate_id, bool blocking, bool silent) {
-    if (*delegate_id >= MAX_NUM_DELEGATES)
-        return false;
-
-    auto delegate = &messaging_delegates_pool[*delegate_id];
-    if (!blocking && delegate->mailbox.isEmpty())
-        return false;
-
-    auto msg = delegate->mailbox.dequeue();
-
-    if (!silent) {
-        delegate->delegate_func(&msg);
     }
 
-    return true;
+    return enqueue_successful ? messaging_send_ok : messaging_send_consumer_buffer_full;
+}
+
+// Consume the next packet in the consumer's buffer
+// If silent is specified will not invoke the callback function
+extern "C" messaging_receive_return_codes messaging_consumer_receive(message_consumer_t* consumer, bool blocking, bool silent) {
+    if (consumer->impl == nullptr)
+        return messaging_receive_invalid_consumer;
+
+    if (consumer->impl->mailbox.isEmpty() && !blocking)
+        return messaging_receive_buffer_empty;
+
+    auto ref = consumer->impl->mailbox.dequeue();
+    if (!silent)
+        consumer->consumer_func(ref->packet);
+
+    return messaging_receive_ok;
 }
