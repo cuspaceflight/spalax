df8de4b10773647e68646869d15fe4c5a530177a
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-11-04 13:32:09 +0000

    Added QUEST algorithm.
    Not fully tested yet, but appears to be working.

--- shared/src/state/quest.cpp
+++ shared/src/state/quest.cpp
@@ -1,9 +1,36 @@
 #include "quest.h"
 #include "Eigen/Core"
+#include <Eigen/Geometry>
 
+// Based on paper at - http://arc.aiaa.org/doi/pdf/10.2514/3.19717
+void quest_estimate(const float observations[2][3], const float references[2][3], const float a[2], float *q_out) {
+    Eigen::Vector3f v1(references[0][0], references[0][1], references[0][2]);
+    Eigen::Vector3f v2(references[1][0], references[1][1], references[1][2]);
 
-void quest_estimate(const float observations[2][3], const float references[2][3], float *q_out) {
-    Eigen::Vector3f observation1(observations[0][0], observations[0][1], observations[0][2]);
-    Eigen::Vector3f observation2(observations[1][0], observations[1][1], observations[1][2]);
+    Eigen::Vector3f w1(observations[0][0], observations[0][1], observations[0][2]);
+    Eigen::Vector3f w2(observations[1][0], observations[1][1], observations[1][2]);
 
+    float lambda_max = v1.dot(v2) * w1.dot(w2) + v1.cross(v2).norm() * w1.cross(w2).norm();
+    Eigen::Matrix3f B = a[0] * (w1 * v1.transpose()) + a[1] * (w2 * v2.transpose());
+
+    Eigen::Matrix3f S = B + B.transpose();
+    float sigma = B.trace();
+    Eigen::Vector3f Z = a[0] * w1.cross(v1) + a[1] * w2.cross(v2);
+
+    auto delta = S.determinant();
+    auto k = S.adjoint().trace();
+
+    auto alpha = lambda_max * lambda_max - sigma * sigma + k;
+    auto beta = lambda_max - sigma;
+    auto gamma = (lambda_max + sigma) * alpha - delta;
+
+    Eigen::Vector3f X = (alpha * Eigen::Matrix3f::Identity() + beta * S + S * S) * Z;
+    auto x_norm = X.norm();
+
+    auto q_mult = 1 / sqrtf(gamma* gamma + x_norm * x_norm);
+
+    q_out[0] = q_mult * X[0];
+    q_out[1] = q_mult * X[1];
+    q_out[2] = q_mult * X[2];
+    q_out[3] = q_mult * gamma;
 }
--- tests/src/TestQuest.cpp
+++ tests/src/TestQuest.cpp
@@ -1,8 +1,27 @@
 #include <gmock/gmock.h>
 #include "state/quest.h"
+#include "Eigen/Core"
+#include <Eigen/Geometry>
 
+bool fuzzy_eq(float A, float B) {
+    const float maxRelativeError = 0.00005f;
+    const float maxAbsoluteError = 1e-5f;
 
-TEST(TestQuest, TestSimple) {
+    if (fabs(A - B) < maxAbsoluteError)
+        return true;
+    float relativeError;
+    if (fabs(B) > fabs(A))
+        relativeError = fabs((A - B) / B);
+    else
+        relativeError = fabs((A - B) / A);
+    return relativeError <= maxRelativeError;
+}
+
+void expect_fuzzy_eq(float A, float B) {
+    EXPECT_TRUE(fuzzy_eq(A, B));
+}
+
+TEST(TestQuest, TestIdentity) {
     const float observations[2][3] = {
             {1.0, 0.0, 0.0f},
             {0.0, 1.0, 0.0f}
@@ -13,10 +32,134 @@ TEST(TestQuest, TestSimple) {
             {0.0, 1.0, 0.0f}
     };
 
+    const float a[2] = {0.5f, 0.5f};
+
 
     float q_out[4];
 
-    quest_estimate(observations, references, q_out);
+    quest_estimate(observations, references, a, q_out);
 
+    expect_fuzzy_eq(q_out[0], 0);
+    expect_fuzzy_eq(q_out[1], 0);
+    expect_fuzzy_eq(q_out[2], 0);
+    expect_fuzzy_eq(q_out[3], 1);
+}
 
+TEST(TestQuest, TestIdentity2) {
+    float observations[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    float references[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    Eigen::Quaternionf rotation(Eigen::AngleAxisf(0.5f, Eigen::Vector3f::UnitZ()));
+
+    for (int i = 0; i < 2; i++) {
+        Eigen::Vector3f orig(observations[i][0], observations[i][1], observations[i][2]);
+        auto new_vec = rotation * orig;
+        observations[i][0] = new_vec[0];
+        observations[i][1] = new_vec[1];
+        observations[i][2] = new_vec[2];
+    }
+
+    for (int i = 0; i < 2; i++) {
+        Eigen::Vector3f orig(references[i][0], references[i][1], references[i][2]);
+        auto new_vec = rotation * orig;
+        references[i][0] = new_vec[0];
+        references[i][1] = new_vec[1];
+        references[i][2] = new_vec[2];
+    }
+
+    const float a[2] = {0.5f, 0.5f};
+
+    float q_out[4];
+
+    quest_estimate(observations, references, a, q_out);
+
+    expect_fuzzy_eq(q_out[0], 0);
+    expect_fuzzy_eq(q_out[1], 0);
+    expect_fuzzy_eq(q_out[2], 0);
+    expect_fuzzy_eq(q_out[3], 1);
+}
+
+TEST(TestQuest, TestIdentity3) {
+    float observations[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    float references[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    Eigen::Quaternionf rotation(Eigen::AngleAxisf(0.25f, Eigen::Vector3f::UnitZ()));
+
+    for (int i = 0; i < 1; i++) {
+        Eigen::Vector3f orig(observations[i][0], observations[i][1], observations[i][2]);
+        auto new_vec = rotation * orig;
+        observations[i][0] = new_vec[0];
+        observations[i][1] = new_vec[1];
+        observations[i][2] = new_vec[2];
+    }
+
+    for (int i = 0; i < 1; i++) {
+        Eigen::Vector3f orig(references[i][0], references[i][1], references[i][2]);
+        auto new_vec = rotation * orig;
+        references[i][0] = new_vec[0];
+        references[i][1] = new_vec[1];
+        references[i][2] = new_vec[2];
+    }
+
+    const float a[2] = {0.5f, 0.5f};
+
+    float q_out[4];
+
+    quest_estimate(observations, references, a, q_out);
+
+    expect_fuzzy_eq(q_out[0], 0);
+    expect_fuzzy_eq(q_out[1], 0);
+    expect_fuzzy_eq(q_out[2], 0);
+    expect_fuzzy_eq(q_out[3], 1);
+}
+
+TEST(TestQuest, TestRotation) {
+    float observations[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    float references[2][3] = {
+            {1.0, 0.0, 0.0f},
+            {0.0, 1.0, 0.0f}
+    };
+
+    Eigen::Quaternionf rotation(Eigen::AngleAxisf(0.3f, Eigen::Vector3f::UnitZ()));
+
+    for (int i = 0; i < 2; i++) {
+        Eigen::Vector3f orig(observations[i][0], observations[i][1], observations[i][2]);
+        auto new_vec = rotation * orig;
+        observations[i][0] = new_vec[0];
+        observations[i][1] = new_vec[1];
+        observations[i][2] = new_vec[2];
+    }
+
+    const float a[2] = {0.5f, 0.5f};
+
+    float q_out[4];
+
+    quest_estimate(observations, references, a, q_out);
+
+    // QUEST returns the quaternion to rotate the observations onto the references
+    // This should be the reverse of the rotation above
+    auto inv_rotation = rotation.inverse();
+
+    expect_fuzzy_eq(q_out[0], inv_rotation.x());
+    expect_fuzzy_eq(q_out[1], inv_rotation.y());
+    expect_fuzzy_eq(q_out[2], inv_rotation.z());
+    expect_fuzzy_eq(q_out[3], inv_rotation.w());
 }
