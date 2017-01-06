00775f46ca104305eb609af1020dd2427c521cc4
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2017-01-06 16:12:57 +0000

    Made component_state compatible with m3_status

--- firmware/src/badthinghandler.c
+++ firmware/src/badthinghandler.c
@@ -31,24 +31,15 @@ static void setSensorOk(bool ok) {
 void bthandler_thread(void* arg) {
     (void)arg;
     chRegSetThreadName("BadThingHandler");
-    const volatile uint8_t* component_states = component_state_get_states();
-    while (true) {
 
-        bool has_error = false;
-        bool has_initializing = false;
-        for (int i = 0; i < avionics_component_max; i++) {
-            avionics_component_state_t state = component_states[i];
-            if (state == state_error) {
-                has_error = true;
-            } else if (state == state_initializing) {
-                has_initializing = true;
-            }
-        }
+    while (true) {
+        avionics_component_state_t state = component_state_get_overall_state();
 #if BAD_THIND_HANDLER_HAS_SENSOR_LEDS
+        const volatile uint8_t* component_states = component_state_get_states();
         setSensorOk(!(component_states[avionics_component_adis16405] == state_error || component_states[avionics_component_mpu9250] == state_error || component_states[avionics_component_ms5611] == state_error));
 #endif
 
-        if (has_error) {
+        if (state == state_error) {
             palClearPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
             palSetPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
             beep_start();
@@ -57,14 +48,14 @@ void bthandler_thread(void* arg) {
             beep_stop();
             chThdSleepMilliseconds(500);
             palClearPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
-        } else if (has_initializing) {
+        } else if (state == state_initializing) {
             palSetPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
             palSetPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
             beep_start();
             chThdSleepMilliseconds(500);
             beep_stop();
             chThdSleepMilliseconds(500);
-        } else {
+        } else if (state == state_ok) {
             palSetPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
             palClearPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
             beep_start();
@@ -72,6 +63,15 @@ void bthandler_thread(void* arg) {
             beep_stop();
             chThdSleepMilliseconds(480);
             palClearPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
+            chThdSleepMilliseconds(500);
+        } else {
+            palSetPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
+            palSetPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
+            beep_start();
+            chThdSleepMilliseconds(500);
+            palClearPad(BAD_THING_HANDLER_NOK_LED_PORT, BAD_THING_HANDLER_NOK_LED_PIN);
+            palClearPad(BAD_THING_HANDLER_OK_LED_PORT, BAD_THING_HANDLER_OK_LED_PIN);
+            beep_stop();
             chThdSleepMilliseconds(500);
         }
     }
